73c719f3fc98fb9c20585ac01927dfc51073921b
Martin Tóth <ezimir@gmail.com>
2019-01-22 10:57:40 +0100

    Complete example render with more complex data structure

--- cv/page.js
+++ cv/page.js
@@ -1,18 +1,10 @@
 
-let obj_mixin = {
-    props: ['o'],
-};
-
-Vue.component('employment', {
-    template: '#vue-employment',
-    mixins: [obj_mixin],
+['employment', 'position', 'project'].map(name => {
+    Vue.component(name, {
+        template: `#vue-${ name }`,
+        props: ['o'],
+    });
 });
-
-Vue.component('project', {
-    template: '#vue-project',
-    mixins: [obj_mixin],
-});
-
 
 var app = new Vue({
     el: '#vue-app',
