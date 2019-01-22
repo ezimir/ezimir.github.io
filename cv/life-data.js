c6e46f8d630d44c6386b4f25e99174aeff77f565
Martin Tóth <ezimir@gmail.com>
2019-01-22 10:57:58 +0100

    Fill out rest of employment data

--- cv/life-data.js
+++ cv/life-data.js
@@ -26,9 +26,185 @@ let WORK = [
                         ],
                     },
                 ],
+            },
+        ],
+    }, {
+        employer: 'Django volunteer',
+        positions: [
+            {
+                title: 'translation coordinator',
+                projects: [
+                    {
+                        title: 'Slovak Translation Team',
+                        duration: '2017 - now',
+                        description: 'coordinate and help with translation effort using Transifex tool',
+                    },
                 ],
             },
         ],
+    }, {
+        employer: 'Blogads, LLC',
+        positions: [
+            {
+                title: 'web developer',
+                projects: [
+                    {
+                        title: 'racery.com',
+                        duration: '2014 - 2017',
+                        description: 'virtual races for companies, charities and clubs',
+                        tasks: [
+                            'spec and implement prototype app',
+                            'create map UI for sharing fitness progress with user profile, leaderboard, messageboard components',
+                            'develop email processing system to allow users to interact with server via email',
+                            'send periodic/on demand templated emails (reminders, leaderboards, ad components)',
+                        ],
+                        tech: [
+                            'Google Maps API, Mailgun API',
+                            'python (django), supervisord, redis, MySQL',
+                            'bootstrap, jQuery, CSS3',
+                            'mercurial',
+                        ],
+                    },
+                ],
+            }, {
+                title: 'prototype developer',
+                projects: [
+                    {
+                        title: 'pullquote.com',
+                        duration: '2012 - 2014',
+                        description: 'quote any content on web and share it via twitter using Chrome extension and iOS app',
+                        note: 'result of previous prototyping employment period',
+                        tasks: [
+                            'create Google Chrome and Firefox (discontinued) extension to allow sharing any text/image content online',
+                            'develop user profile pages (with tagging, statistics and email subscription capabilities)',
+                        ],
+                        tech: [
+                            'python (Django 1), custom API solution',
+                            'Chrome/Firefox extension',
+                            'supervisord, redis, MySQL, MongoDB',
+                            'wkhtmltoimage, PIL',
+                            'apache, wsgi, mercurial',
+                        ],
+                    }, {
+                        title: 'multiple projects',
+                        duration: '2012',
+                        description: [
+                            'lunchbag.me (meeting/scheduling app)',
+                            'truthfill.com (people-tagging/organizational app)',
+                            'distillry.com (twitter search/stream based analytics)',
+                            'twialog.com (twitter follower analytics)',
+                            'instagram timeline visualisation',
+                        ],
+                        tasks: [
+                            'quickly create prototype apps to determine feasability of project functionality',
+                        ],
+                        tech: [
+                            'Twitter and Instagram APIs',
+                            'python (Django)',
+                            'bootstrap, jQuery',
+                            'mercurial',
+                        ],
+                    },
+                ],
+            },
+        ],
+    }, {
+        employer: 'Pressflex Kft.',
+        positions: [
+            {
+                title: 'junior web developer',
+                projects: [
+                    {
+                        title: 'taghive.com',
+                        duration: '2011 - 2012',
+                        description: 'gamification of tweeting via tagging people',
+                        note: 'project discontinued',
+                        tasks: [
+                            'spec and create Twitter analysis tool using #hashtags as base of processing',
+                            'read relevant content from Twitter Stream and Search AP',
+                            'develop timeline/volume/heatmap visualisation',
+                            'implement badge/award assignment algorhythms based on time, frequency and volume of content',
+                        ],
+                        tech: [
+                            'Twitter Search and Stream API',
+                            'python (Django), celery',
+                        ],
+                    }, {
+                        title: 'twiangulate.com',
+                        duration: '2010 - 2011',
+                        description: 'Twitter follower analysis tool',
+                        tasks: [
+                            'refactor UI to incorporate modern design',
+                            'improve search usability',
+                            'learn python, django',
+                        ],
+                        tech: [
+                            'python (django)',
+                            'bootstrap, jQuery',
+                            'svn, trac',
+                        ],
+                    }, {
+                        title: 'collatr.org',
+                        duration: '2009 - 2010',
+                        description: 'RSS feed aggregator',
+                        tasks: [
+                            'improve in-browser CSS style editor',
+                        ],
+                        tech: [
+                            'jQuery, HTML',
+                            'svn',
+                        ],
+                    },
+                ],
+            },
+        ],
+    }, {
+        employer: 'Micronic s.r.o.',
+        positions: [
+            {
+                title: 'multi purpose programmer',
+                projects: [
+                    {
+                        duration: '2009',
+                        tasks: [
+                            'redesign company web page with limited CMS capabilities',
+                        ],
+                        tech: [
+                            'PHP, plain HTML/CSS/JS stack',
+                            'svn', 'trac',
+                        ],
+                    }, {
+                        duration: '2007',
+                        description: '',
+                        tasks: [
+                            'create command line tool for tabular display of printed circuit design CAD data',
+                        ],
+                        tech: [
+                            'C#',
+                            'svn',
+                        ],
+                    },
+                ],
+            },
+        ],
+    }, {
+        employer: 'Technical University in Košice',
+        positions: [
+            {
+                title: 'web developer',
+                projects: [
+                    {
+                        duration: '2005 - 2007',
+                        tasks: [
+                            'develop web page with PHP photogallery (project APIC)',
+                            'create web pages for (project ANPROBA)',
+                        ],
+                        tech: [
+                            'PHP, plain HTML/CSS/JS stack',
+                        ],
+                    },
+                ],
+            },
         ],
     },
 ];
